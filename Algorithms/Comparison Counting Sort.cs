180014f4bad390ee72bf60adeadf331e9886bc05
Carlos Casalicchio <carlos.casalicchio@arvos.com>
2017-01-10 13:19:17 -0200

    added more algorithms

---
+++ Algorithms/Comparison Counting Sort.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+
+//namespace Algorithms
+//{
+//    public class Comparison_Counting_Sort
+//    {
+//        public static void Main()
+//        {
+//            new Comparison_Counting_Sort().Run(Console.In, Console.Out);
+//        }
+
+//        public void Run(TextReader reader, TextWriter writer)
+//        {
+//            int[] array = Setup();
+
+//            var results = ComparisonCountingSort(array);
+//            foreach (var result in results)
+//                writer.Write(result + ",");
+
+//            Console.ReadLine();
+
+//        }
+
+//        public int[] Setup()
+//        {
+//            return new int[] { 23, 43, 293, 493, 22, 233, 12, 1, 42, 56, 9, 84, 92, 32, 54, 67 };
+//        }
+//        /// <summary>
+//        /// Sorts an array by comparison couting
+//        /// </summary>
+//        /// <param name="A">Array A[0..n-1] of orderable values</param>
+//        /// <returns>Array S[0..n-1] of A's elemements sorted in nondecreasing order</returns>
+//        public Array ComparisonCountingSort(int[] A)
+//        {
+//            int[] count = new int[A.Length];
+//            int[] S = new int[A.Length];
+
+//            for (int i = 0; i < A.Length - 1; i++)
+//                count[i] = 0;
+
+//            for (int i = 0; i < A.Length - 2; i++)
+//            {
+//                for (int j = i + 1; j < A.Length - 1; j++)
+//                {
+//                    if (A[i] < A[j])
+//                        count[j]++;
+//                    else count[i]++;
+
+//                }
+//            }
+//            for (int i = 0; i < A.Length - 1; i++)
+//                S[count[i]] = A[i];
+
+//            return S;
+//        }
+//    }
+//}
--- Algorithms/Euclid.cs
+++ Algorithms/Euclid.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.IO;
+﻿//using System;
+//using System.IO;
 
-namespace Algorithms
-{
-    public class Euclid
-    {
-        public static void Main()
-        {
-            new Euclid().Run(Console.In, Console.Out);
-        }
+//namespace Algorithms
+//{
+//    public class Euclid
+//    {
+//        public static void Main()
+//        {
+//            new Euclid().Run(Console.In, Console.Out);
+//        }
 
-        public void Run(TextReader reader, TextWriter writer)
-        {
-            int m = int.Parse(reader.ReadLine());
-            int n = int.Parse(reader.ReadLine());
+//        public void Run(TextReader reader, TextWriter writer)
+//        {
+//            int m = int.Parse(reader.ReadLine());
+//            int n = int.Parse(reader.ReadLine());
 
-            var results = GCD(m, n);
-            writer.Write(results);
+//            var results = GCD(m, n);
+//            writer.Write(results);
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
-        /// <summary>
-        /// Computes Greates Common Divisor by Euclids Algorithm
-        /// Input: Two nonnegative, not-both-zero integers m and n
-        /// Output: Greatest common divisor of m and n
-        /// </summary>
-        /// <param name="m">First Number</param>
-        /// <param name="n">Second Number</param>
-        /// <returns>Greatest Common Divisor or m and n</returns>
-        public int GCD(int m, int n)
-        {
-            while (n != 0)
-            {
-                int r = m % n;
-                m = n;
-                n = r;
-            }
-            return m;
-        }
-    }
-}
+//        }
+//        /// <summary>
+//        /// Computes Greates Common Divisor by Euclids Algorithm
+//        /// Input: Two nonnegative, not-both-zero integers m and n
+//        /// Output: Greatest common divisor of m and n
+//        /// </summary>
+//        /// <param name="m">First Number</param>
+//        /// <param name="n">Second Number</param>
+//        /// <returns>Greatest Common Divisor or m and n</returns>
+//        public int GCD(int m, int n)
+//        {
+//            while (n != 0)
+//            {
+//                int r = m % n;
+//                m = n;
+//                n = r;
+//            }
+//            return m;
+//        }
+//    }
+//}
