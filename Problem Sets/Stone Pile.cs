ff94db65b059b16ff4c0d18152848d76c891b968
Carlos Casalicchio <carlos.casalicchio@arvos.com>
2017-01-26 16:37:30 -0200

    Stone Pile Attempt

--- Problem Sets/Stone Pile.cs
+++ Problem Sets/Stone Pile.cs
@@ -1,61 +1,63 @@
-﻿//using System;
-//using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-///// <summary>
-///// From http://acm.timus.ru/problem.aspx?space=1&num=1005
-///// Find the total possible combinations for the number of stones and create that number of piles
-///// Create an array with all possible weights and sort by weight, get the lowest value
-///// Still not right, use napsac problem
-///// </summary>
-//namespace Algorithms
-//{
-//    public class Stone_Pile
-//    {
-//        private int numberOfStones;
-//        private int[] stones;
+/// <summary>
+/// From http://acm.timus.ru/problem.aspx?space=1&num=1005
+/// Time limit: 1.0 second
+/// Memory limit: 64 MB
+/// You have a number of stones with known weights w1, …, wn.Write a program that will rearrange the stones into two piles such that weight difference between the piles is minimal.
+/// Input
+/// Input contains the number of stones n(1 ≤ n ≤ 20) and weights of the stones w1, …, wn(integers, 1 ≤ wi ≤ 100000) delimited by white spaces.
+/// Output
+/// Your program should output a number representing the minimal possible weight difference between stone piles.
+/// input   
+/// 5
+/// 5 8 13 27 14
+/// output
+/// 3
+/// Problem Source: USU Championship 1997
+/// Tags: problem for beginners
+/// Difficulty: 92 
+/// </summary>
+namespace Algorithms
+{
+    public class Stone_Pile
+    {
+        public void Run(TextReader reader, TextWriter writer)
+        {
+            //First line = number of stones
+            int numberOfStones = int.Parse(reader.ReadLine());
 
-//        public void Run(TextReader reader, TextWriter writer)
-//        {
-//            numberOfStones = int.Parse(reader.ReadLine());
-//            string[] weights = reader.ReadLine().Split(' ');
+            //Second line = weights of stones
+            List<int> weights = ParseStones(reader.ReadLine());
 
-//            stones = new int[numberOfStones];
+            writer.Write(SortPiles(weights, numberOfStones));
 
-//            for (int i = 0; i < stones.Length; i++)
-//                stones[i] = int.Parse(weights[i]);
+            reader.ReadLine();
 
-//            stones = SortStonesByWeight(stones);
+        }
+        public List<int> ParseStones(string stones)
+        {
+            return stones.Split(' ').Select(Int32.Parse).ToList();
+        }
+        public int SortPiles(List<int> weights, int numberOfStones)
+        {
+            int pileOne, pileTwo;
 
-//            int sizeOfPiles = (int)Math.Ceiling((numberOfStones / 2.0));
-//            int pileOneWeight = 0, pileTwoWeight = 0;
+            pileOne = 0;
+            pileTwo = 0;
 
-//            for (int i = stones.Length - 1; i >= 0; i--)
-//            {
-//                if (pileOneWeight <= pileTwoWeight || pileOneWeight == pileTwoWeight)
-//                    pileOneWeight += stones[i];
-//                else
-//                    pileTwoWeight += stones[i];
-//            }
+            //Sort by lowest weight
+            weights.Sort();
 
-//            writer.WriteLine((int)Math.Abs(Math.Round(pileOneWeight - pileTwoWeight / 1.0)));
-//        }
+            return -1;
 
-//        public int[] SortStonesByWeight(int[] stones)
-//        {
-//            for (int i = 1; i < stones.Length; i++)
-//            {
-//                int weight = stones[i];
-//                if (weight < stones[i - 1])
-//                {
-//                    stones[i] = stones[i - 1];
-//                    stones[i - 1] = weight;
-//                }
-//            }
-//            return stones;
-//        }
-//        public static void Main()
-//        {
-//            new Stone_Pile().Run(Console.In, Console.Out);
-//        }
-//    }
-//}
+        }
+        public static void Main()
+        {
+            new Stone_Pile().Run(Console.In, Console.Out);
+        }
+    }
+}
