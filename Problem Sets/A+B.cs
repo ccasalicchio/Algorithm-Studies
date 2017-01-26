2f7c40a564beaf1ee14c416b940b085284add837
Carlos Casalicchio <carlos.casalicchio@arvos.com>
2017-01-26 15:39:14 -0200

    Added Problems Sets

---
+++ Problem Sets/A+B.cs
@@ -0,0 +1,12 @@
+//using System;
+///// <summary>
+///// http://acm.timus.ru/problem.aspx?space=1&num=1000
+///// </summary>
+//public class Sum
+//{
+//    private static void Main()
+//    {
+//        string[] values = Console.ReadLine().Split(' ');
+//        Console.WriteLine(int.Parse(values[0]) + int.Parse(values[1]));
+//    }
+//}
